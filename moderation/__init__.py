435766575e0002b4d4fd80ba43ea1105a960f861
MAR PD <mdranasheikhe2005@gmail.com>
2025-12-23 16:06:22 +0600

    Add files via upload

--- moderation/__init__.py
+++ moderation/__init__.py
@@ -1,1 +1,27 @@
+"""
+Blue Rose Bot - Moderation Module
+Message moderation and spam protection
+"""
 
+__version__ = "1.0.0"
+__author__ = "RANA (MASTER)"
+
+from .anti_spam import AntiSpam
+from .anti_flood import AntiFlood
+from .anti_link import AntiLink
+from .anti_forward import AntiForward
+from .anti_bot import AntiBot
+from .auto_warn import AutoWarn
+from .auto_mute import AutoMute
+from .auto_ban import AutoBan
+
+__all__ = [
+    'AntiSpam',
+    'AntiFlood',
+    'AntiLink',
+    'AntiForward',
+    'AntiBot',
+    'AutoWarn',
+    'AutoMute',
+    'AutoBan',
+]
